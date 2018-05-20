3837d9f9aa24e412127dc0fbfdf6c52e36622047
Govind <go2307@gmail.com>
2018-05-20 15:23:02 +0530

    blank

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
-  istrue:true;
-  myName:string='enter Name';
-  sum1:number=0;
-   Num:number=0;
-   result:number=0;
-   
+ 
 
 
-  abc()
-  {
-    alert(this.myName);
-  }
-
-  sum()
-  {
-    this.sum1=(+this.sum1)+(+this.Num);
-     return this.sum1;
-  }
-
-  myfunc()
-
-  {
-  this.result=this.sum();
-  if(this.result!=0)
-  {
-    return true;
-  }
-  else if(this.result==0 && this.result==undefined)
-  {
-   return false;
-  }
-  }
 }
+
+   
