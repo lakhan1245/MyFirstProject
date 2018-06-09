2cd8f32046a2e8f6c471ad230158a43744d94b5c
Govind <go2307@gmail.com>
2018-06-09 12:13:35 +0530

    routing example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
+import { RoutingModuleModule } from './routing-module/routing-module.module';
+import { DefaultComponent } from './default/default.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C1Component,
+    C2Component,
+    C3Component,
+    DefaultComponent,
+    PagenotfoundComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,RoutingModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing-module/routing-module.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { C1Component } from '../c1/c1.component';
+import { C2Component } from '../c2/c2.component';
+import { C3Component } from '../c3/c3.component';
+import { DefaultComponent } from '../default/default.component';
+import { PagenotfoundComponent } from '../pagenotfound/pagenotfound.component';
+
+
+
+const routes:Routes=[
+{path:'c1',component:C1Component},
+{path:'c2',component:C2Component},
+{path:'c3',component:C3Component},
+{path:'',component:DefaultComponent},
+{path:'**',component:PagenotfoundComponent},
+
+
+];
+
+@NgModule({
+  imports: [
+
+
+    RouterModule.forRoot(routes)
+  
+  ],
+  exports:[RouterModule],
+  declarations: []
+})
+export class RoutingModuleModule { }
