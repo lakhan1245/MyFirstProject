a7fd59034792c080c793c7e8531eb5341fb9528e
Govind <go2307@gmail.com>
2018-06-30 11:56:50 +0530

    dont take

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,FormsModule
